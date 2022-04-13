bf66e0e3ad47d4e517f716fc8d3dc2a3579ec8cb
weiwei.wu@ninebot.com <weiwei.wu@ninebot.com>
2022-04-13 18:45:42 +0800

    add Regular Expression Matching

---
+++ leetcode/src/RegularExpressionMatching.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegularExpressionMatching {
+    public static void main(String[] args) {
+        RegularExpressionMatching instance = new RegularExpressionMatching();
+        System.out.println(instance.isMatch("aa", "aa*bb.*cc"));
+    }
+    public boolean isMatch(String s, String p) {
+        int startIndex = -1;
+        boolean isVerb = false;
+        List<String> parts = new ArrayList<String>();
+        for (int i = 0, n = p.length(); i < n; i++) {
+            if (startIndex == -1) {
+                startIndex = i;
+                isVerb = 'a' <= p.charAt(i) && p.charAt(i) <= 'z';
+            } else {
+                if (!isVerb && 'a' <= p.charAt(i) && p.charAt(i) <= 'z') {
+
+                }
+            }
+        }
+        return false;
+    }
+    private boolean isMatchVariable(String s, String p) {
+
+        return false;
+    }
+}
