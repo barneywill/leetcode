c9dd7caef11a90ca11f6557d48cdcc468c9d9665
weiwei.wu@ninebot.com <weiwei.wu@ninebot.com>
2022-04-21 19:25:16 +0800

    optimize Find First And Last Position Of Element In Sorted Array

--- leetcode/src/FindFirstAndLastPositionOfElementInSortedArray.java
+++ leetcode/src/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -18,18 +18,18 @@ public class FindFirstAndLastPositionOfElementInSortedArray {
         int start = -1;
         int end = -1;
         if (nums.length > 0) {
-            int index = Arrays.binarySearch(nums, target - 1);
+            int index = Arrays.binarySearch(nums, target);
             if (index >= 0) {
                 start = index;
                 end = index;
 
-                if (index > 0) {
+                if (index > 0 && nums[index - 1] == target) {
                     int nextStart = findNext(nums, target, 0, index - 1, true);
                     if (nextStart != -1 && nextStart < start) {
                         start = nextStart;
                     }
                 }
-                if (index < nums.length - 1) {
+                if (index < nums.length - 1 && nums[index + 1] == target) {
                     int nextEnd = findNext(nums, target, index + 1,  nums.length - 1, false);
                     if (nextEnd != -1 && nextEnd > end) {
                         end = nextEnd;
@@ -40,32 +40,28 @@ public class FindFirstAndLastPositionOfElementInSortedArray {
         return new int[]{start, end};
     }
     private int findNext(int[] nums, int target, int start, int end, boolean isBackward) {
-        if (start > end) {
-            return -1;
-        } else if (start == end) {
-            if (nums[start] == target) {
-                return start;
-            } else {
-                return -1;
-            }
-        } else if (target < nums[start] || target > nums[end]) {
-            return -1;
-        }
         if (isBackward && nums[start] == target) {
             return start;
         } else if (!isBackward && nums[end] == target) {
             return end;
         }
+        if (start + 1 == end) {
+            if (isBackward) {
+                return end;
+            } else {
+                return start;
+            }
+        }
 
         int middle = (start + end) / 2;
         if (isBackward) {
-            if (nums[middle] >= target) {
+            if (nums[middle] == target) {
                 return findNext(nums, target, start, middle, isBackward);
             } else {
                 return findNext(nums, target, middle, end, isBackward);
             }
         } else {
-            if (nums[middle] <= target) {
+            if (nums[middle] == target) {
                 return findNext(nums, target, middle, end, isBackward);
             } else {
                 return findNext(nums, target, start, middle, isBackward);
