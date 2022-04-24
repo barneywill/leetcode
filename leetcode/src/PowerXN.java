4f3271541c4c126da96f96a3e616a2954379009d
weiwei.wu@ninebot.com <weiwei.wu@ninebot.com>
2022-04-24 22:58:46 +0800

    optimize Power X N optimize Trapping Rain Water

--- leetcode/src/PowerXN.java
+++ leetcode/src/PowerXN.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PowerXN {
     public static void main(String[] args) {
         PowerXN instance = new PowerXN();
@@ -6,6 +9,7 @@ public class PowerXN {
         System.out.println(instance.myPow(2.00000, -2));
         System.out.println(instance.myPow(1.00000, -2));
         System.out.println(instance.myPow(2.00000, 0));
+        System.out.println(instance.myPow(1.72777, 7));
     }
     public double myPow(double x, int n) {
         double result = 1;
@@ -16,9 +20,32 @@ public class PowerXN {
         } else if (x != 1 && n != 0) {
             boolean isPositive = n > 0;
             int positiveN = n == Integer.MIN_VALUE ? Integer.MAX_VALUE : Math.abs(n);
-            while (positiveN-- > 0) {
-                result *= x;
-                if (!isPositive && result > 1000000) {
+            int currentN = positiveN;
+            List<XNPair> values = new ArrayList<XNPair>();
+            int i = 2;
+            while (currentN > 0) {
+                if (currentN == positiveN) {
+                    result *= x;
+                    values.add(new XNPair(1, result));
+                    currentN = positiveN - 1;
+                } else if (currentN > positiveN - currentN) {
+                    result *= result;
+                    values.add(0, new XNPair(i, result));
+                    currentN = positiveN - i;
+                    i *= 2;
+                } else {
+                    while (values.size() > 0) {
+                        while (currentN >= values.get(0).n) {
+                            result *= values.get(0).x;
+                            currentN -= values.get(0).n;
+                            if (!isPositive && result > 100000) {
+                                return 0;
+                            }
+                        }
+                        values.remove(0);
+                    }
+                }
+                if (!isPositive && result > 100000) {
                     return 0;
                 }
             }
@@ -27,5 +54,13 @@ public class PowerXN {
             }
         }
         return result;
+    }
+    class XNPair {
+        public XNPair(int n, double x){
+            this.n = n;
+            this.x = x;
+        }
+        public int n = 0;
+        public double x = 0;
     }
 }
--- leetcode/src/TrappingRainWater.java
+++ leetcode/src/TrappingRainWater.java
@@ -1,35 +1,65 @@
-public class TrappingRainWater {
+import java.util.Arrays;
 
+public class TrappingRainWater {
+    public static void main(String[] args) {
+        TrappingRainWater instance = new TrappingRainWater();
+        System.out.println(instance.trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
+        System.out.println(instance.trap(new int[]{4,2,0,3,2,5}));
+    }
     public int trap(int[] height) {
         int result = 0;
-        int[] sortHeight = new int[height.length];
-        int maxHeight = 0;
-        int maxHeightStart = -1;
-        int maxHeightEnd = -1;
-        for (int i = 0; i < height.length; i++) {
-            sortHeight[i] = height[i];
-            if (maxHeight < height[i]) {
-                maxHeight = height[i];
-                maxHeightStart = i;
-                maxHeightEnd = i;
-            } else (maxHeight == height[i]) {
-                maxHeightEnd = i;
+        if (height.length > 1) {
+            int maxHeight = 0;
+            int maxHeightStart = -1;
+            int maxHeightEnd = -1;
+            int minHeight = Integer.MAX_VALUE;
+            for (int i = 0; i < height.length; i++) {
+                if (height[i] > 0) {
+                    if (maxHeight < height[i]) {
+                        maxHeight = height[i];
+                        maxHeightStart = i;
+                        maxHeightEnd = i;
+                    } else if (maxHeight == height[i]) {
+                        maxHeightEnd = i;
+                    }
+                    if (minHeight > height[i]) {
+                        minHeight = height[i];
+                    }
+                }
+            }
+//            System.out.println("min : " + minHeight + ", max : " + maxHeight + ", " + maxHeightStart + "-" + maxHeightEnd);
+            int[] sortHeight = new int[maxHeight - minHeight];
+            for (int i = 0; i < height.length; i++) {
+                if (height[i] > 0 && height[i] < maxHeight) {
+                    sortHeight[height[i] - minHeight]++;
+                }
+            }
+            result += getWater(maxHeightStart, maxHeightEnd, maxHeight, height);
+//            System.out.println("result : " + result);
+            if (maxHeightStart != 0 || maxHeightEnd != height.length - 1) {
+                HeightState hs = new HeightState();
+                hs.start = maxHeightStart;
+                hs.end = maxHeightEnd;
+                for (int i = sortHeight.length - 1; i >= 0; i--) {
+                    if (sortHeight[i] > 0) {
+                        hs.height = i + minHeight;
+//                        System.out.println("start : " + hs.start + ", end : " + hs.end + ", " + hs.height);
+                        result += expand(height, hs);
+//                        System.out.println("result : " + result);
+                        if (hs.start == 0 && hs.end == height.length - 1) {
+                            break;
+                        }
+                    }
+                }
             }
         }
-        Arrays.sort(sortHeight);
-        if (maxHeightStart < maxHeightEnd) {
-            result = (maxHeightEnd - maxHeightStart) * maxHeight;
-        }
-        HeightState hs = new HeightState();
-        hs.start = maxHeightStart;
-        hs.end = maxHeightEnd;
-        if (maxHeightStart > 0 || maxHeightEnd < height.length - 1) {
-            for (int i = sortHeight.length - 2; i >= 0; i++) {
-                hs.height = sortHeight[i];
-                result += expand(height, hs);
-                if (hs.start == 0 && hs.end = height.length - 1) {
-                    break;
-                }
+        return result;
+    }
+    private int getWater(int start, int end, int maxHeight, int[] height) {
+        int result = 0;
+        if (start + 1 < end) {
+            for (int i = start + 1; i < end; i++) {
+                result += (maxHeight - height[i]);
             }
         }
         return result;
@@ -38,23 +68,43 @@ public class TrappingRainWater {
         int result = 0;
         int start = -1;
         int end = -1;
-        for (int i = 0; i < start; i++) {
+        boolean isEmpty = true;
+        for (int i = 0; i < hs.start; i++) {
+            if (height[i] > 0) {
+                isEmpty = false;
+            }
             if (height[i] == hs.height) {
                 start = i;
                 break;
             }
         }
-        for (int i = height.length - 1; i > end; i--) {
+        if (start != -1) {
+            result += getWater(start, hs.start, hs.height, height);
+            hs.start = start;
+        } else if (isEmpty) {
+            hs.start = 0;
+        }
+        isEmpty = true;
+        for (int i = height.length - 1; i > hs.end; i--) {
+            if (height[i] > 0) {
+                isEmpty = false;
+            }
             if (height[i] == hs.height) {
                 end = i;
                 break;
             }
         }
+        if (end != -1) {
+            result += getWater(hs.end, end, hs.height, height);
+            hs.end = end;
+        } else if (isEmpty) {
+            hs.end = height.length - 1;
+        }
         return result;
     }
-    class HeightState (
-            public int height = 0;
-            public int start = 0;
-            public int end = 0;
-    )
+    class HeightState {
+        public int height = 0;
+        public int start = 0;
+        public int end = 0;
+    }
 }
