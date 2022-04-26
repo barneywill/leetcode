8fa3b0b89383249ff8b38e4e009fb9e7f83b828c
weiwei.wu@ninebot.com <weiwei.wu@ninebot.com>
2022-04-26 21:30:18 +0800

    optimize Maximum Subarray optimize Permutation Sequence

--- leetcode/src/MaximumSubarray.java
+++ leetcode/src/MaximumSubarray.java
@@ -5,26 +5,64 @@ public class MaximumSubarray {
         System.out.println(instance.maxSubArray(new int[]{1}));
         System.out.println(instance.maxSubArray(new int[]{-1}));
         System.out.println(instance.maxSubArray(new int[]{-2, -1}));
+        System.out.println(instance.maxSubArray(new int[]{-1, -2}));
+        System.out.println(instance.maxSubArray(new int[]{-1, 0, -2}));
+        System.out.println(instance.maxSubArray(new int[]{0, -3, 1, 1}));
         System.out.println(instance.maxSubArray(new int[]{5,4,-1,7,8}));
+        System.out.println(instance.maxSubArray(new int[]{9,0,-2,-2,-3,-4,0,1,-4,5,-8,7,-3,7,-6,-4,-7,-8}));
     }
     public int maxSubArray(int[] nums) {
-        int result = 0;
+        int result = Integer.MIN_VALUE;
         int sum = 0;
         if (nums.length == 1) {
             result = nums[0];
         } else {
             int minSum = Integer.MAX_VALUE;
+            int minSumIndex = 0;
             int maxSum = Integer.MIN_VALUE;
+            int maxSumIndex = 0;
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            int[] sums = new int[nums.length];
             for (int i = 0; i < nums.length; i++) {
                 sum += nums[i];
+                sums[i] = sum;
+                if (min > nums[i]) {
+                    min = nums[i];
+                }
+                if (max < nums[i]) {
+                    max = nums[i];
+                }
                 if (minSum > sum) {
                     minSum = sum;
+                    minSumIndex = i;
                 }
                 if (maxSum < sum) {
                     maxSum = sum;
+                    maxSumIndex = i;
                 }
             }
-            result = minSum > 0 ? maxSum : maxSum - minSum;
+//            System.out.println("" + minSum + ", " + minSumIndex + ", " + maxSum + ", " + maxSumIndex);
+            if (max <= 0) {
+                result = max;
+            } else if (minSum > 0) {
+                result = maxSum;
+            } else if (minSumIndex < maxSumIndex) {
+                result = maxSum - minSum;
+            } else {
+                result = Math.max(maxSum, max);
+                int currentMin = Integer.MAX_VALUE;
+                for (int i = 0; i < sums.length - 1; i++) {
+                    if (sums[i] < 0 && sums[i] < currentMin) {
+                        currentMin = sums[i];
+                        for (int j = i + 1; j < sums.length; j++) {
+                            if (sums[j] > sums[i] && result < sums[j] - sums[i]) {
+                                result = sums[j] - sums[i];
+                            }
+                        }
+                    }
+                }
+            }
         }
         return result;
     }
--- leetcode/src/PermutationSequence.java
+++ leetcode/src/PermutationSequence.java
@@ -2,43 +2,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PermutationSequence {
-    //TODO:
     public static void main(String[] args) {
         PermutationSequence instance = new PermutationSequence();
         System.out.println(instance.getPermutation(3, 3));
         System.out.println(instance.getPermutation(4, 9));
-        System.out.println(instance.getPermutation(3, 3));
+        System.out.println(instance.getPermutation(3, 1));
+        System.out.println(instance.getPermutation(2, 2));
     }
     public String getPermutation(int n, int k) {
         StringBuffer result = new StringBuffer();
         if (n == 1) {
             result.append("1");
+        } else if (n == 2) {
+            if (k == 1) {
+                result.append("12");
+            } else {
+                result.append("21");
+            }
         } else {
             int[] count = new int[n - 1];
-            List<Integer> list = new ArrayList<Integer>();
+            List<Byte> list = new ArrayList<Byte>();
             for (int i = 0; i < n - 1; i++) {
-                list.add(i + 1);
+                list.add((byte)(i + 1));
                 if (i == 0) {
                     count[i] = 1;
                 } else {
                     count[i] = count[i - 1] * (i + 1);
                 }
             }
-            list.add(n);
-            for (int c : count) {
-                System.out.print(c + ",");
-            }
-            System.out.println();
-            for (int i = n - 2; i >= 0; i--) {
-                System.out.println("result : " + result + ", " + k);
-                int devide = k / count[i];
-                result.append(list.remove(devide));
-                k = k % count[i];
-                if (k == 0) {
-                    for (Integer left : list) {
-                        result.append(left);
+            list.add((byte)n);
+//            for (int c : count) {
+//                System.out.print(c + ",");
+//            }
+//            System.out.println();
+            k -= 1;
+            if (k > 0) {
+                for (int i = n - 2; i >= 0; i--) {
+//                System.out.println("result : " + result + ", " + k);
+                    if (k == 0) {
+                        break;
                     }
+                    int divide = k / count[i];
+                    result.append(list.remove(divide));
+                    k = k % count[i];
                 }
+            }
+            for (Byte left : list) {
+                result.append(left);
             }
         }
         return result.toString();
