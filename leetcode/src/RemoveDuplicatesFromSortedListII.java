adfa9c4148234f84e7e8886e7db80de150b87cc3
weiwei.wu@ninebot.com <weiwei.wu@ninebot.com>
2022-05-11 10:28:30 +0800

    add Remove Duplicates From Sorted List II

--- leetcode/src/RemoveDuplicatesFromSortedListII.java
+++ leetcode/src/RemoveDuplicatesFromSortedListII.java
@@ -4,23 +4,50 @@ public class RemoveDuplicatesFromSortedListII extends BaseListNode {
         RemoveDuplicatesFromSortedListII instance = new RemoveDuplicatesFromSortedListII();
         print(instance.deleteDuplicates(generate(new int[]{1,2,3,3,4,4,5})));
         print(instance.deleteDuplicates(generate(new int[]{1,1,1,2,3})));
+        print(instance.deleteDuplicates(generate(new int[]{1,1,1})));
+        print(instance.deleteDuplicates(generate(new int[]{1,2,2})));
+        print(instance.deleteDuplicates(generate(new int[]{1})));
     }
     public ListNode deleteDuplicates(ListNode head) {
+        //newHead: when newHead == null and previous != current and previous != lastDuplicateValue then newHead = previous
+        //newTail: when previous != current and previous != lastDuplicateValue then newTail = previous, last previous, cross newTail
+        //previous -> current: when previous == current set the lastDuplicateValue
+        //lastDuplicateValue
+
         ListNode current = head;
         ListNode previous = null;
         ListNode newHead = null;
-        ListNode tail = null;
+        ListNode newTail = null;
+        int duplicateValue = -200;
         while (current != null) {
             if (previous == null) {
-                previous = current;
-                current = current.next;
             } else if (current.val == previous.val) {
-                current = current.next;
-                previous.next = current;
+                duplicateValue = current.val;
             } else {
-                previous = current;
-                current = current.next;
+                if (duplicateValue != previous.val) {
+                    if (newHead == null) {
+                        newHead = previous;
+                        newTail = previous;
+                    } else {
+                        newTail.next = previous;
+                        newTail = newTail.next;
+                    }
+                }
             }
+            previous = current;
+            current = current.next;
+        }
+        if (previous != null && duplicateValue != previous.val) {
+            if (newHead == null) {
+                newHead = previous;
+                newTail = previous;
+            } else {
+                newTail.next = previous;
+                newTail = newTail.next;
+            }
+        }
+        if (newTail != null) {
+            newTail.next = null;
         }
         return newHead;
     }
