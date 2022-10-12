36581698e8b6c03d4778b3ee3c60c221453fb0e6
weiwei.wu@ninebot.com <weiwei.wu@ninebot.com>
2022-10-12 10:37:26 +0800

    add Binary Tree Level Order Traversal

---
+++ leetcode/src/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeLevelOrderTraversal extends BaseTreeNode {
+    public static void main(String[] args) {
+        BinaryTreeLevelOrderTraversal instance = new BinaryTreeLevelOrderTraversal();
+        System.out.println(instance.levelOrder(generate(new Integer[]{3,9,20,null,null,15,7})));
+        System.out.println(instance.levelOrder(generate(new Integer[]{3})));
+        System.out.println(instance.levelOrder(generate(new Integer[]{})));
+    }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root != null) {
+            List<TreeNode> nodeList = new ArrayList<TreeNode>();
+            nodeList.add(root);
+            travel(result, nodeList);
+        }
+        return result;
+    }
+    private void travel(List<List<Integer>> result, List<TreeNode> nodeList) {
+        List<Integer> list = new ArrayList<Integer>();
+        List<TreeNode> nextNodeList = new ArrayList<TreeNode>();
+        for (TreeNode node : nodeList) {
+            list.add(node.val);
+            if (node.left != null) {
+                nextNodeList.add(node.left);
+            }
+            if (node.right != null) {
+                nextNodeList.add(node.right);
+            }
+        }
+        result.add(list);
+        if (!nextNodeList.isEmpty()) {
+            travel(result, nextNodeList);
+        }
+    }
+}
