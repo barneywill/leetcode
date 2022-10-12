d0260a2bd2fae41592a4c385ab9e534c28f2a593
weiwei.wu@ninebot.com <weiwei.wu@ninebot.com>
2022-10-12 14:26:02 +0800

    add Max Depth of Binary Tree

---
+++ leetcode/src/MaxDepthOfBinaryTree.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MaxDepthOfBinaryTree extends BaseTreeNode {
+    public static void main(String[] args) {
+        MaxDepthOfBinaryTree instance = new MaxDepthOfBinaryTree();
+        System.out.println(instance.maxDepth(generate(new Integer[]{3,9,20,null,null,15,7})));
+        System.out.println(instance.maxDepth(generate(new Integer[]{1,null,2})));
+        System.out.println(instance.maxDepth(generate(new Integer[]{1})));
+        System.out.println(instance.maxDepth(generate(new Integer[]{})));
+    }
+    public int maxDepth(TreeNode root) {
+        int result = 0;
+        List<TreeNode> list = new ArrayList<TreeNode>();
+        if (root != null) {
+            list.add(root);
+            do {
+                result++;
+                list = travelNext(list);
+            } while (!list.isEmpty());
+        }
+        return result;
+    }
+    private List<TreeNode> travelNext(List<TreeNode> nodeList) {
+        List<TreeNode> result = new ArrayList<TreeNode>();
+        for (TreeNode node : nodeList) {
+            if (node.left != null) {
+                result.add(node.left);
+            }
+            if (node.right != null) {
+                result.add(node.right);
+            }
+        }
+        return result;
+    }
+}
