0e94af7aec7606ce26fcb5cc71b5a9d8408d3958
barneywill <jarodwill@163.com>
2024-11-19 13:10:59 +0100

    update two_sum

--- leetcode/src/python/two_sum.py
+++ leetcode/src/python/two_sum.py
@@ -3,12 +3,11 @@ from typing import List
 class TwoSum:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(0, len(nums) - 1):
-            numOne = nums[i]
-            numTwo = target - nums[i]
+            num = target - nums[i]
             for j in range(i + 1, len(nums)):
-                if nums[j] == numTwo:
+                if nums[j] == num:
                     return [i, j]
-        return None
+        return []
 
 if __name__ == '__main__':
     nums = [2, 7, 11, 15]
